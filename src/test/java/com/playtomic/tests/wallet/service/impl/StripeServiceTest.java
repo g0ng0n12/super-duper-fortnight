e1ff040d4267e7f11e2d4a499cf9c94baf33dc04
Gon Gis <g0ng0n.dev@gmail.com>
2022-02-12 01:48:45 +0100

    modif the import in stripe tst because I moved the service into a folder

--- src/test/java/com/playtomic/tests/wallet/service/impl/StripeServiceTest.java
+++ src/test/java/com/playtomic/tests/wallet/service/impl/StripeServiceTest.java
@@ -1,9 +1,9 @@
 package com.playtomic.tests.wallet.service.impl;
 
 
-import com.playtomic.tests.wallet.service.StripeAmountTooSmallException;
-import com.playtomic.tests.wallet.service.StripeServiceException;
-import com.playtomic.tests.wallet.service.StripeService;
+import com.playtomic.tests.wallet.service.stripe.StripeAmountTooSmallException;
+import com.playtomic.tests.wallet.service.stripe.StripeServiceException;
+import com.playtomic.tests.wallet.service.stripe.StripeService;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
